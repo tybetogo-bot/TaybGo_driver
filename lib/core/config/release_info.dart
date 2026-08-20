c118eb6c402b09fe2f2596f5066777228d5092e3
abdulzilah <65471371+abdulzilah@users.noreply.github.com>
2026-08-21 00:04:12 +0300

    Sync 1.0.14 release metadata

--- lib/core/config/release_info.dart
+++ lib/core/config/release_info.dart
@@ -2,5 +2,5 @@ class ReleaseInfo {
   ReleaseInfo._();
 
   static const String version = '1.0.14+16';
-  static const String releaseDate = '20 August 2026';
+  static const String releaseDate = '21 August 2026';
 }
